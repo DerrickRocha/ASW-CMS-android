36ed2f08014eb487ce7c3f8809d46ecb3b7e2c70
derrick <drocha616@gmail.com>
2026-01-20 20:34:39 -0700

    Added consumeWindowInsets to SplashScreen modifier.

--- app/src/main/java/com/example/aswcms/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/example/aswcms/ui/splash/SplashScreen.kt
@@ -4,8 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,7 +40,7 @@ fun SplashScreen(viewModel: SplashViewModel = viewModel(), onShowNextScreen: () 
 
 @Composable
 fun SplashContent() {
-    Surface(modifier = Modifier.fillMaxSize()) {
+    Surface(modifier = Modifier.fillMaxSize().windowInsetsPadding(WindowInsets.safeDrawing)) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
             Row(
                 modifier = Modifier,
