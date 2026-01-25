dddffd655ed14dc39a67dee8d900bb782947ccb5
derrick <drocha616@gmail.com>
2026-01-25 01:37:39 -0700

    Removed compose annotation.

--- app/src/main/java/com/example/aswcms/extensions/MainMenuItemExtensions.kt
+++ app/src/main/java/com/example/aswcms/extensions/MainMenuItemExtensions.kt
@@ -1,10 +1,8 @@
 package com.example.aswcms.extensions
 
-import androidx.compose.runtime.Composable
 import com.example.aswcms.R
 import com.example.aswcms.ui.viewmodels.MainMenuItem
 
-@Composable
 fun MainMenuItem.resolveMainMenuItemString(): Int {
     return when (this) {
         MainMenuItem.Account -> R.string.account
